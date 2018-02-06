72d8ea8b365b175adf56a0c48d27a60c5bc099b3
shuixianbing <shui6666@126.com>
2018-02-06 11:52:42 +0800

    优化监听对象,返回home界面,一般在2-3秒内抢到

--- app/src/main/java/com/bshui/hongbaowx/HongBaoService.java
+++ app/src/main/java/com/bshui/hongbaowx/HongBaoService.java
@@ -13,8 +13,6 @@ import android.view.accessibility.AccessibilityNodeInfo;
 import java.util.List;
 
 public class HongBaoService extends AccessibilityService {
-    public HongBaoService() {
-    }
 
     @Override
     protected void onServiceConnected() {
@@ -33,10 +31,11 @@ public class HongBaoService extends AccessibilityService {
             case AccessibilityEvent.TYPE_NOTIFICATION_STATE_CHANGED:
                 handleNotification(accessibilityEvent);
                 break;
+
             //监听是否进入微信红包消息界面
             case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED:
                 String className = accessibilityEvent.getClassName().toString();
-                Log.i("bshui","className:"+className);
+
                 if(className.equals("com.tencent.mm.ui.LauncherUI")){
 
                     //点击还没有拆过的红包,自已发的是查看红包,别人发的是领取红包
@@ -44,48 +43,45 @@ public class HongBaoService extends AccessibilityService {
                         return;
 
                     getPacket(rootNodeInfo);
+
+
                 }else if(className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI")){
                     //进入开红包界面,点击拆红包,根据id找节点
                     openPacket();
                 }else if(className.equals("com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI")){
                     //拆开红包后进入红包详情页返回聊天界面
 
-                   // backPacket();
-                    //GLOBAL_ACTION_BACK
-                    //修改成返回Home界面才能接收到下一次的红包通知
+                    backPacket();
+                    //GLOBAL_ACTION_HOME
+                    //修改成返回GLOBAL_ACTION_HOME界面才能接收到下一次的红包通知
                     performGlobalAction(AccessibilityService.GLOBAL_ACTION_HOME);
                 }
 
                 break;
-                //在微信列表内但没有在微信界面,进入微信界面
-            case AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED:
-              //  Log.i("bshui","TYPE_WINDOW_CONTENT_CHANGED");
-                break;
 
-            case AccessibilityEvent.TYPE_WINDOWS_CHANGED:
-                Log.i("bshui","TYPE_WINDOWS_CHANGED");
-                break;
+
+
         }
     }
 
+
+
+
     //模拟点击,打开抢红包界面
-    private void getPacket(AccessibilityNodeInfo rootNodeInfo){
+    private void getPacket(AccessibilityNodeInfo rootNodeInfo) {
         List<AccessibilityNodeInfo> nodes = rootNodeInfo.findAccessibilityNodeInfosByText("领取红包");
 
-        if (!nodes.isEmpty()){
-            //判断nodes有几个,每个都要模拟点击,从最新的开始点
-            for(int i=nodes.size(); i>0; i--){
-                Log.i("bshui","nodes.size:"+nodes.size()+" name:"+nodes.get(i-1).getParent().getClassName().toString());
-                nodes.get(i-1).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            }
+        if (!nodes.isEmpty()) {
+            //判断nodes有几个,点击最新的一个
 
+           // Log.i("bshui", "get Red Packet:" + nodes.size() );
+            nodes.get(nodes.size() - 1).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+        } else {
 
-        }else{
-            Log.i("bshui","nodes is empty");
             List<AccessibilityNodeInfo> mynodes = rootNodeInfo.findAccessibilityNodeInfosByText("查看红包");
-            if(!mynodes.isEmpty()) {
-                if (mynodes.size() > 0)
-                    mynodes.get(mynodes.size() - 1).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            if (!mynodes.isEmpty()) {
+             //   Log.i("bshui", "mySelf Red Packet");
+                mynodes.get(mynodes.size() - 1).getParent().performAction(AccessibilityNodeInfo.ACTION_CLICK);
             }
         }
     }
@@ -98,21 +94,23 @@ public class HongBaoService extends AccessibilityService {
         if(nodeInfo !=null){
             List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/c2i");
 
-            if(list.get(0).getClassName().toString().equals("android.widget.Button")){
+            //if(list.get(0).getClassName().toString().equals("android.widget.Button")){
                 list.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
-            }
+           // }
 
         }
     }
+
     //从红包返回界面
     private void backPacket(){
         AccessibilityNodeInfo nodeInfo = getRootInActiveWindow();
         if(nodeInfo !=null){
             //找到返回的id
             List<AccessibilityNodeInfo> list = nodeInfo.findAccessibilityNodeInfosByViewId("com.tencent.mm:id/ho");
-
-            if(list.get(0).getClassName().toString().equals("android.widget.LinearLayout")){
-                list.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+            if(list.size() > 0) {
+               // if (list.get(0).getClassName().toString().equals("android.widget.LinearLayout")) {
+                    list.get(0).performAction(AccessibilityNodeInfo.ACTION_CLICK);
+               // }
             }
 
         }
@@ -130,13 +128,12 @@ public class HongBaoService extends AccessibilityService {
                 String context = text.toString();
                 //[微信红包]恭喜发财，大吉大利
                 if (context.contains("微信红包")) {
-                    //打开通知栏状态
+                    //打开通知栏状态,进入相应的聊天窗口
                     if (event.getParcelableData() != null && event.getParcelableData() instanceof Notification) {
                         Notification notification = (Notification) event.getParcelableData();
                         PendingIntent pendingIntent = notification.contentIntent;
                         try {
                             pendingIntent.send();
-
                         } catch (PendingIntent.CanceledException e) {
                             e.printStackTrace();
                         }
@@ -147,8 +144,9 @@ public class HongBaoService extends AccessibilityService {
     }
 
     @Override
-    public void onInterrupt() {
-        Log.i("bshui","onInterrupt");
+    public void onInterrupt()
+    {
+
     }
 
     @Override
